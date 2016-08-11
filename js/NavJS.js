f31b0b5db9d44e035cd7a3f429453e82d9fa304b
CollusWang <colluswang@163.com>
2016-08-11 17:23:33 +0800

    add js directory

---
+++ js/NavJS.js
@@ -0,0 +1,12 @@
+// window.onload=function () {
+// 	var iLi = document.getElementsByTagName('li');
+// 	for (var i = 0; i < iLi.length; i++) {
+// 		iLi[i].onmouseover=function(){
+// 			this.className='lihover';
+// 		}
+// 		iLi[i].onmouseout=function(){
+// 			this.className='';
+// 		}
+// 	}
+
+// }
